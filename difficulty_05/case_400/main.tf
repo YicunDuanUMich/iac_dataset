4cb608ebb9c88f195cdfd789f29989f564ce0cb8
YicunDuanUMich <pduan@umich.edu>
2024-12-28 00:39:35 +0000

    fix bugs in diff 05

--- difficulty_05/case_400/main.tf
+++ difficulty_05/case_400/main.tf
@@ -1,50 +1,100 @@
-# Define the provider block for AWS
-provider "aws" {
-  region = "us-east-2" # Set your desired AWS region
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.75"
+    }
+  }
+
+  required_version = "~> 1.9.8"
 }
 
+provider "aws" {
+  region  = "us-east-1"
+  profile = "admin-1"
+
+  assume_role {
+    role_arn = "arn:aws:iam::590184057477:role/yicun-iac"
+  }
+}
+
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+
+  name = "main-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs                  = data.aws_availability_zones.available.names
+  public_subnets       = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["*ubuntu-noble-24.04-amd64-server-*"]
+  }
+}
+
+resource "aws_security_group" "instance-sg" {
+  name = "instance-sg"
+  vpc_id = module.vpc.vpc_id
+}
+
+resource "aws_vpc_security_group_ingress_rule" "instance-sg-ingress-rule" {
+  from_port       = 0
+  to_port         = 0
+  ip_protocol     = "-1"
+  cidr_ipv4   = "0.0.0.0/0"
+  security_group_id = aws_security_group.instance-sg.id
+}
+
+resource "aws_vpc_security_group_egress_rule" "instance-sg-egress-rule" {
+  from_port       = 0
+  to_port         = 0
+  ip_protocol     = "-1"
+  cidr_ipv4       = "0.0.0.0/0"
+  security_group_id = aws_security_group.instance-sg.id
+}
 
 resource "aws_instance" "my_instance" {
-  ami           = "ami-06d4b7182ac3480fa" # Replace with your desired AMI ID
-  instance_type = "t2.micro"     # Replace with your desired instance type
-  subnet_id     = aws_subnet.example1.id
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+  subnet_id     = module.vpc.public_subnets[0]
+  security_groups = [ aws_security_group.instance-sg.id ]
 }
 
 resource "aws_lb" "test" {
   load_balancer_type = "gateway"
-  subnets            = [aws_subnet.example1.id, aws_subnet.example2.id]
-}
-
-resource "aws_subnet" "example1" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.1.0/24"
-  availability_zone = "us-east-2a"
-}
-
-resource "aws_subnet" "example2" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.2.0/24"
-  availability_zone = "us-east-2b"
-}
-
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
+  subnet_mapping {
+    subnet_id = module.vpc.public_subnets[0]
+  }
 }
 
 resource "aws_lb_listener" "my_listener" {
   load_balancer_arn = aws_lb.test.arn
 
   default_action {
-    target_group_arn = aws_lb_target_group.target_group.arn
     type             = "forward"
+    target_group_arn = aws_lb_target_group.target_group.arn
   }
-
 }
 
 resource "aws_lb_target_group" "target_group" {
-  port     = 80
-  protocol = "HTTP"
-  vpc_id   = aws_vpc.main.id
+  port     = 6081
+  protocol = "GENEVE"
+  vpc_id   = module.vpc.vpc_id
+
+  health_check {
+    port     = 80
+    protocol = "HTTP"
+  }
 }
 
 resource "aws_lb_target_group_attachment" "ec2_attach" {
--- difficulty_05/case_427/main.tf
+++ difficulty_05/case_427/main.tf
@@ -2,38 +2,43 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.16"
+      version = "~> 5.75"
     }
   }
 
-  required_version = ">= 1.2.0"
+  required_version = "~> 1.9.8"
 }
-# Define the provider block for AWS
+
 provider "aws" {
-  region = "us-east-2" # Set your desired AWS region
+  region  = "us-east-1"
+  profile = "admin-1"
+
+  assume_role {
+    role_arn = "arn:aws:iam::590184057477:role/yicun-iac"
+  }
+}
+
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+locals {
+  subnet_count = 2
 }
 
 resource "aws_vpc" "_" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_hostnames = true
-  tags = merge({
-    "Name" = "vpc"
-  })
 }
 
 resource "aws_subnet" "public" {
-  count             = 1
+  count             = local.subnet_count
   vpc_id            = aws_vpc._.id
-  cidr_block        = [
-                      "10.0.1.0/24",
+  cidr_block        = ["10.0.1.0/24",
                       "10.0.2.0/24",
                       "10.0.3.0/24",
-                      "10.0.4.0/24"
-                    ][count.index]
-  availability_zone = "us-west-2"
-  tags = merge({
-    "Name" = "public-subnet-${count.index}"
-  })
+                      "10.0.4.0/24"][count.index]
+  availability_zone = data.aws_availability_zones.available.names[count.index]
 }
 
 resource "aws_internet_gateway" "_" {
@@ -46,13 +51,10 @@ resource "aws_route_table" "public" {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway._.id
   }
-  tags = merge({
-    "Name" = "public-rt"
-  })
 }
 
 resource "aws_route_table_association" "public" {
-  count          = 2
-  subnet_id      = aws_subnet.public[0].id
+  count          = local.subnet_count
+  subnet_id      = aws_subnet.public[count.index].id
   route_table_id = aws_route_table.public.id
 }
